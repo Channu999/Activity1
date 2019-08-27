3fab74063e6b4691cca29a5c33e89665922bdf75
1893288 <chandanvolkaji99@gmail.com>
2019-08-27 16:41:08 -0400

    navigation

--- app/src/main/java/com/example/activity1/MainActivity.java
+++ app/src/main/java/com/example/activity1/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.activity1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+    public Toolbar toolbar;
+
+    public DrawerLayout drawerLayout;
+
+    public NavController navController;
+
+    public NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigation();
+
+    }
+
+    // Setting Up One Time Navigation
+    private void setupNavigation() {
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.navigationView);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(drawerLayout, Navigation.findNavController(this, R.id.nav_host_fragment));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        menuItem.setChecked(true);
+
+        drawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+
+        switch (id) {
+
+            case R.id.first:
+                navController.navigate(R.id.firstFragment);
+                break;
+
+            case R.id.second:
+                navController.navigate(R.id.secondFragment);
+                break;
+
+            case R.id.third:
+                navController.navigate(R.id.thirdFragment);
+                break;
+
+        }
+        return true;
+
     }
 }
